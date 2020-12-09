60a753b4ab244e7c9959710980ff4941060bf59e
ahmdali25 <ahmadaliazhary@gmail.com>
2020-12-09 16:59:55 +0700

    Menambahkan file kasus yang lain

---
+++ novice/01-02/kasus/kasus-10/main.js
@@ -0,0 +1,9 @@
+var a = '';
+for (var i = 0; i < 6; i++){
+    for (var j = 0; j < i; j++){
+        a += '*';        
+    }
+    a += '\n';
+}
+
+console.log(a);
---
+++ novice/01-02/kasus/kasus-6/main.js
@@ -0,0 +1,22 @@
+var students = [['Ali', 80], ['Albab', 77], ['Andi', 88], ['Viko', 94], ['Evan', 78]];
+var Avgmarks = 0;
+
+for (var i=0; i < students.length; i++) {
+    Avgmarks += students[i][1];
+    var avg = (Avgmarks/students.length);
+}
+
+console.log("Average grade: " + (Avgmarks)/students.length);
+    if (avg < 60){
+        console.log("Grade : F");      
+    } 
+    else if (avg < 70) {
+        console.log("Grade : D"); 
+    } 
+    else if (avg < 80) {
+        console.log("Grade : C"); 
+    } else if (avg < 90) {
+        console.log("Grade : B"); 
+    } else if (avg < 100) {
+        console.log("Grade : A"); 
+}
