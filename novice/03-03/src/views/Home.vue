f9bd12bba2d318ca6cfc70867e429e2a9e4b79f2
ahmdali25 <ahmadaliazhary@gmail.com>
2020-12-24 22:05:04 +0700

    menambahkan latihan dan kasus lifecycle hooks

---
+++ novice/03-03/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <LifeCycle life="life-cycle"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import LifeCycle from '@/components/LifeCycle.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    LifeCycle
+  }
+}
+</script>
