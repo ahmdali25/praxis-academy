f0f21e43dc70e0d1a1e622063026f57845173edb
ahmdali25 <ahmadaliazhary@gmail.com>
2020-12-30 14:22:14 +0700

    menambahkan latihan state management dengan vuex

---
+++ novice/04-03/src/store/index.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  // store state
+  state: {
+    count: 0,
+    users: []
+  },
+  // untuk mengganti state di store
+  mutations: {
+    // method untuk increment value properti dari count
+    increment (state) {
+      state.count++
+    },
+    // method untuk decrement value properti dari count
+    decrement (state) {
+      state.count--
+    },
+    // method untuk mendapatkan value dari properti users
+    getUsers (state, data) {
+      state.users = data;
+    }
+  },
+  // untuk menghitung state yang diturunkan dari store state
+  getters: {
+    getUpUsers: state => {
+      return state.users.filter(item => item.id <= 10)
+    }
+  },
+  // actions mengkomit mutations, dapat berisi operasi asynchronous
+  actions: {
+    increment (context) {
+      context.commit('increment')
+    },
+    
+    decrement (context) {
+      context.commit('decrement')
+    },
+
+    async users({commit}) {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        const data = response.data;
+        commit('getUsers', data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  modules: {
+  }
+})
---
+++ novice/04-03/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Learn Vuex</h1>
+    <!-- Button untuk increment dan decrement value dari count -->
+    <button @click="addCount">Increment</button> |
+    <button @click="subtractCount">Decrement</button>
+    <!-- Untuk mendapatkan value dari store state count  -->
+    <p>{{ $store.state.count }}</p>
+    <!-- Perulangan untuk mengulang value dari store state users berdasarkan nama dari user -->
+    <ul v-for="user in $store.state.users" :key="user.name">
+      <li>{{ user.name }}</li>
+    </ul>
+    <!-- <li>{{ $store.state.users }}</li> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  },
+
+  methods: {
+    // method yang digunakan untuk mengambil store yang dikirimkan dari actions
+    addCount() {
+      this.$store.dispatch('increment')
+    },
+
+    subtractCount() {
+      this.$store.dispatch('decrement')
+    },
+
+    getUsers(){
+      this.$store.dispatch('users')
+    }
+  },
+  // method untuk membuat instance
+  created() {
+    this.getUsers();
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+  }
+</style>
